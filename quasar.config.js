fef4b4a37ab620285444a6cdf42a4eea9bf1305b
bobkuo <geneteamgou@gmail.com>
2025-08-28 14:25:11 +0800

    部屬設定2

--- quasar.config.js
+++ quasar.config.js
@@ -46,7 +46,7 @@ export default defineConfig((ctx) => {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: './',
+      publicPath: '/final-front',
       // analyze: true,
       // env: {},
       // rawDefine: {}
