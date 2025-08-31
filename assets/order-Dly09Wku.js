135931585c56bf7bd636c8c87211b2f662fd46d7
BobKuo <geneteamgou@gmail.com>
2025-08-31 10:50:58 +0000

    Deploying to gh-pages from @ BobKuo/final-front@230b8bfce66cfc09e6adacaeacd747c82437bce5 🚀

--- assets/order-Dly09Wku.js
+++
@@ -1,1 +0,0 @@
-import{b2 as r}from"./index-DuVc2JOo.js";const t={create(){return r.apiAuth.post("/order")},getMy(){return r.apiAuth.get("/order/my")},getAll(){return r.apiAuth.get("/order/all")}};export{t as o};
