58267aa5830ef21c0af06304c2004ca4e619f7f2
bobkuo <geneteamgou@gmail.com>
2025-09-01 20:43:44 +0800

    首頁RWD

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,18 +2,20 @@
   <!-- <q-page class="flex flex-center">
     <q-img src="~assets/home.png" fit="contain" style="height: calc(100vh - 100px)"></q-img>
   </q-page> -->
-  <div class="row justify-center text-center bg-judy-1" style="height: calc(100vh - 100px)">
-    <div class="col-4">
-      <q-img src="~assets/logo.png" />
-    </div>
-    <div class="col-12 q-gutter-lg">
-      <div class="text-h2 text-judy-2 intro_title">I'm Judy</div>
-      <div class="text-h6 about-description">
-        <p>Judy Wang，本名王君儀，台灣台北人。</p>
-        <p>Judy擅長色彩鮮明、可愛療癒的畫風，注重作品的通俗性、趣味性以及讀者的互動性。</p>
+  <q-page class="flex flex-center bg-judy-1">
+    <div class="row justify-center text-center">
+      <div class="col-12 col-sm-8">
+        <q-img src="~assets/logo.png" />
+      </div>
+      <div class="col-12 col-sm-8 q-gutter-lg">
+        <div class="text-h2 text-judy-2 intro_title">I'm Judy</div>
+        <div class="text-h6 about-description">
+          <p>Judy Wang，本名王君儀，台灣台北人。</p>
+          <p>Judy擅長色彩鮮明、可愛療癒的畫風，注重作品的通俗性、趣味性以及讀者的互動性。</p>
+        </div>
       </div>
     </div>
-  </div>
+  </q-page>
 </template>
 <script setup></script>
 <style scoped>
