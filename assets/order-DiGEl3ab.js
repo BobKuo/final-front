e93aba69c7a672e110ccaa5accde734724b3c7ed
BobKuo <geneteamgou@gmail.com>
2025-09-02 17:43:03 +0000

    Deploying to gh-pages from @ BobKuo/final-front@59a0fdc9de75004fc2b00a1db38017c3d81925c9 🚀

---
+++ assets/order-DiGEl3ab.js
@@ -0,0 +1,1 @@
+import{b7 as r}from"./index-Com1BDuJ.js";const t={create(){return r.apiAuth.post("/order")},getMy(){return r.apiAuth.get("/order/my")},getAll(){return r.apiAuth.get("/order/all")}};export{t as o};
