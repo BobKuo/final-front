2ffe07b2eecb822a31bea7039fc9ca1d929d5019
BobKuo <geneteamgou@gmail.com>
2025-09-02 20:53:11 +0000

    Deploying to gh-pages from @ BobKuo/final-front@0d8a0b37ab6a6ea4c7cb71075b1a4de3dfeaba4f 🚀

---
+++ assets/series-CvtANb9a.js
@@ -0,0 +1,1 @@
+import{b7 as r}from"./index-CWWtOGS4.js";const i={create(e){return r.apiAuth.post("/series/add",e)},getAll(){return r.apiAuth.get("/series/all")},get(){return r.api.get("/series")},getId(e){return r.api.get("/series/"+e)},update(e,t){return r.apiAuth.patch(`/series/${e}`,t)}};export{i as s};
